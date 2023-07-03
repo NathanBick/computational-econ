82d1640e7cc2cf8f497e30ebf4dfae722d3173ab
Nathan Bick <nathankbick1@gmail.com>
2023-07-03 08:32:11 -0400

    initial simulations running

---
+++ plotting.R
@@ -0,0 +1,62 @@
+library(ggplot2)
+library(dplyr)
+options(scipen=999)
+
+# read in the data
+data <- read.csv("simulation_data.csv", header = TRUE)
+
+# make the data long on money, happiness
+data_long <- data %>%
+    tidyr::gather(key = "variable", value = "value", hours,money, happiness)
+
+# calculate the total amount of money, time, and happiness for each type in each step
+data_long_totals <- data_long %>%
+    group_by(type, variable, step) %>%
+    summarise(value = sum(value))
+
+# add across type as well
+data_long_totals2 <- data_long_totals %>%
+    group_by(variable, step) %>%
+    summarise(value = sum(value))
+
+data_long_totals2 %>%
+    ggplot(aes(x=step, y=value, color=variable)) +
+    geom_line() +
+    theme_bw() +
+    theme(legend.position = "bottom") +
+    labs(x = "Step", y = "Total value")
+    
+# plot the means amount of money, time, and happiness for each type in each step
+p <- data_long %>%
+    group_by(type, variable, step) %>%
+    summarise(mean = mean(value)) %>%
+    ggplot(aes(x = step, y = mean, color = variable)) +
+    geom_line() +
+    facet_wrap(~type, ncol = 1) +
+    theme_bw() +
+    theme(legend.position = "bottom") +
+    labs(x = "Step", y = "Mean value")
+
+# open p in a new window
+ggsave("plots/plot.png", p, width = 10, height = 10, units = "in", dpi = 300)
+
+# plot the totals
+p_tot <- data_long_totals %>%
+    ggplot(aes(x = step, y = value, color = variable)) +
+    geom_line() +
+    facet_wrap(~type, ncol = 1) +
+    theme_bw() +
+    theme(legend.position = "bottom") +
+    labs(x = "Step", y = "Total value")
+
+# open p in a new window
+ggsave("plots/plot_tot.png", p_tot, width = 10, height = 10, units = "in", dpi = 300)
+
+# get distribution of money and happiness at the beginning of the simulation
+data %>%
+    filter(step == 0) %>%
+    ggplot(aes(x = money)) +
+    geom_histogram(bins = 100) +
+    facet_grid(~type) +
+    theme_bw() +
+    labs(x = "Money", y = "Count")
--- simulation.py
+++ simulation.py
@@ -3,15 +3,27 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import numpy as np  
+import pandas as pd
+
+num_nodes = 100
+num_iterations = 100
+
+#### NOTES to self
+# currently, there is an early drop in money - where does this come from?
+# need to look more closely at the trade function
 
 # create a graph
 G = nx.DiGraph()
 
 # add 100 nodes with labels 0, 1, 2, ..., 99
-G.add_nodes_from(range(100))
+G.add_nodes_from(range(num_nodes))
+
+# add id variable to each node
+for i in range(num_nodes):
+    G.nodes[i]['id'] = i
 
 # now we assign a type variable to each node, red, blue, or white
-for i in range(100):
+for i in range(num_nodes):
     if i < 20:
         G.nodes[i]['type'] = 'red'
     elif i < 50:
@@ -25,8 +37,8 @@ for i in range(100):
 # if the pair is red and blue, the probability is 0.2
 # if one of the nodes is white, the probability is 0.01
 # the edges are undirected, so we add both directions
-for i in range(100):
-    for j in range(100):
+for i in range(num_nodes):
+    for j in range(num_nodes):
         if i != j:
             if G.nodes[i]['type'] == G.nodes[j]['type']:
                 if random.random() < 0.4:
@@ -50,50 +62,136 @@ for i in range(100):
 # for red, the money N(1000, 100)
 # for blue, the money is N(500, 100) 
 # for white, the money is N(100, 10)
-for i in range(100):
+for i in range(num_nodes):
     G.nodes[i]['hours'] = 8
     if G.nodes[i]['type'] == 'red':
-        G.nodes[i]['money'] = np.random.normal(1000, 100)
+        G.nodes[i]['money'] = np.random.normal(1000, 10)
     elif G.nodes[i]['type'] == 'blue':
-        G.nodes[i]['money'] = np.random.normal(500, 100)
+        G.nodes[i]['money'] = np.random.normal(500, 10)
     else:
         G.nodes[i]['money'] = np.random.normal(100, 10)
 
 # now we define the production and utility functions for the nodes
+# the production function: money = beta1 * hours + beta2 * money + beta3 * neighbors' money + beta0
+# the utility function: utility = alpha1 * hours + alpha2 * money + alpha3 * neighbors' money + alpha0
+# the parameters are chosen randomly from truncated normal distributions with mean 0.5, sd 1, and truncated at 0 and 1
+for i in range(num_nodes):
+    # if outside range of 0 and 1, truncate
+    G.nodes[i]['beta1'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['beta2'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['beta3'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['beta0'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['alpha1'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['alpha2'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['alpha3'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
+    G.nodes[i]['alpha0'] = max(min(max(np.random.normal(0.5,1), 0), 1), 0)
 
+# now we define the production and utility functions
+# for now we do not include the neighbors' information in the calculation
+def production(node):
+    return G.nodes[node]['beta1'] * G.nodes[node]['hours'] + G.nodes[node]['beta2'] * G.nodes[node]['money'] + G.nodes[node]['beta0']
+    #return G.nodes[node]['beta1'] * G.nodes[node]['hours'] + G.nodes[node]['beta2'] * G.nodes[node]['money'] + G.nodes[node]['beta3'] * sum([G.nodes[i]['money'] for i in G.neighbors(node)]) + G.nodes[node]['beta0']
+
+def utility(node):
+    return G.nodes[node]['alpha1'] * G.nodes[node]['hours'] + G.nodes[node]['alpha2'] * G.nodes[node]['money'] + G.nodes[node]['alpha0']
+    #return G.nodes[node]['alpha1'] * G.nodes[node]['hours'] + G.nodes[node]['alpha2'] * G.nodes[node]['money'] + G.nodes[node]['alpha3'] * sum([G.nodes[i]['money'] for i in G.neighbors(node)]) + G.nodes[node]['alpha0']
+
+# now we define the update function across the nodes
+def update():
+    for i in range(num_nodes):
+        G.nodes[i]['money'] = production(i)
+        G.nodes[i]['happiness'] = utility(i)
+
+# define trade function
+# this function takes in two nodes, and transfers either money or time from one to the other
+# the max of hours is 16
+# for each combo of ndoes, check if a trade will make both nodes better off - if so, do it
+# price is the price of 1 hour of time in terms of money
+def trade(node1, node2, price):
+    # check utility of node1 and node2 before trade
+    utility1 = utility(node1)
+    utility2 = utility(node2)
+
+    # attempt to trade in each direction
+    # if the trade makes both nodes better off, do it
+    # node 1 sells time to node 2
+    if G.nodes[node1]['hours'] > 0 and G.nodes[node2]['money'] > 0:
+        G.nodes[node1]['hours'] -= 1
+        G.nodes[node2]['hours'] += 1
+        G.nodes[node1]['money'] += price
+        G.nodes[node2]['money'] -= price
+        if utility(node1) < utility1 and utility(node2) < utility2:
+            G.nodes[node1]['hours'] += 1
+            G.nodes[node2]['hours'] -= 1
+            G.nodes[node1]['money'] -= price
+            G.nodes[node2]['money'] += price
+    # node 2 sells time to node 1
+    # if G.nodes[node2]['hours'] > 0 and G.nodes[node1]['money'] > 0:
+    #     G.nodes[node2]['hours'] -= 1
+    #     G.nodes[node1]['hours'] += 1
+    #     G.nodes[node2]['money'] += price
+    #     G.nodes[node1]['money'] -= price
+    #     if utility(node1) < utility1 and utility(node2) < utility2:
+    #         G.nodes[node2]['hours'] += 1
+    #         G.nodes[node1]['hours'] -= 1
+    #         G.nodes[node2]['money'] -= price
+    #         G.nodes[node1]['money'] += price
+
+# now we define the simulation
+# we run the simulation for 1000 steps
+# at each step, we update the nodes, and then we trade
+# crate a dataframe to store the data
+data = []
+for i in range(num_iterations):
+    update()            
+    for j in range(num_nodes):
+        for k in G.neighbors(j):
+            trade(j, k, 10)
+    print(i)
+    # save the data for each node to a long dataframe
+    for j in range(num_nodes):
+        data.append([i, j, G.nodes[j]['hours'], G.nodes[j]['money'], G.nodes[j]['type'], G.nodes[j]['happiness']])
+
+# write the data to a csv file
+with open('simulation_data.csv', 'w') as f:
+    # add names to the columns
+    fieldnames = ['step', 'id', 'hours', 'money', 'type', 'happiness']
+    df = pd.DataFrame(data)
+    df.columns = fieldnames
+    df.to_csv(f, header=True, index=False)
 
 # describe the distributions of money across the nodes
 # we can do this by plotting a histogram
 
-# first we get the money values
-money = [G.nodes[i]['money'] for i in range(100)]
+# # first we get the money values
+# money = [G.nodes[i]['money'] for i in range(100)]
 
-# now we plot the histogram
-plt.hist(money, bins=20)
-plt.savefig("money.png")
-# clear
-plt.clf()   
+# # now we plot the histogram
+# plt.hist(money, bins=20)
+# plt.savefig("money.png")
+# # clear
+# plt.clf()   
 
-# draw the graph and save to file
-# draw the nodes by the type
-# red nodes are circles, blue nodes are squares, white nodes are triangles
-# the size of the node is proportional to the amount of money
+# # draw the graph and save to file
+# # draw the nodes by the type
+# # red nodes are circles, blue nodes are squares, white nodes are triangles
+# # the size of the node is proportional to the amount of money
 
-# make sure there isnt overlap between nodes
-pos = nx.spring_layout(G)
+# # make sure there isnt overlap between nodes
+# pos = nx.spring_layout(G)
 
-# node size is proportional to money
-nx.draw_networkx_nodes(G, pos, nodelist=[i for i in range(100) if G.nodes[i]['type'] == 'red'], node_color='r', node_shape='o')
-nx.draw_networkx_nodes(G, pos, nodelist=[i for i in range(100) if G.nodes[i]['type'] == 'blue'], node_color='b', node_shape='s')
-nx.draw_networkx_nodes(G, pos, nodelist=[i for i in range(100) if G.nodes[i]['type'] == 'white'], node_color='g', node_shape='^')
+# # node size is proportional to money
+# nx.draw_networkx_nodes(G, pos, nodelist=[i for i in range(100) if G.nodes[i]['type'] == 'red'], node_color='r', node_shape='o')
+# nx.draw_networkx_nodes(G, pos, nodelist=[i for i in range(100) if G.nodes[i]['type'] == 'blue'], node_color='b', node_shape='s')
+# nx.draw_networkx_nodes(G, pos, nodelist=[i for i in range(100) if G.nodes[i]['type'] == 'white'], node_color='g', node_shape='^')
 
-# draw the edges
-nx.draw_networkx_edges(G, pos)
+# # draw the edges
+# nx.draw_networkx_edges(G, pos)
 
-# save to file
-# save to a large file so we can zoom in    
-plt.savefig("graph.png", dpi=1000)
+# # save to file
+# # save to a large file so we can zoom in    
+# plt.savefig("graph.png", dpi=1000)
 
-# save graph to file
-nx.write_gexf(G, "graph.gexf")
+# # save graph to file
+# nx.write_gexf(G, "graph.gexf")
 
